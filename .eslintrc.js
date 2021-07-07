396e289d43469210dd45227d4519b1fa8811b404
Clayton <corman.dev@gmail.com>
2021-07-07 06:47:51 -0400

    :wrench: include apollo libraries

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,21 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@react-native-community',
+  ],
+  rules: {
+    // '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    // '@typescript-eslint/no-unused-vars-experimental': 'error',
+    // '@typescript-eslint/no-use-before-define': 'off',
+    // 'no-unused-vars': 'off',
+    // '@typescript-eslint/no-explicit-any': 'off',
+    // 'react-hooks/exhaustive-deps': 'off',
+    // 'react-native/no-inline-styles': 'off'
+  },
 };
--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {FC} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+import {GRAPHQL_URI} from '@env';
+
 import Icon from './src/components/shared/icon';
 import ManageScreen from './src/screens/manage';
 import CategoryListStackScreen from './src/screens/category-list';
@@ -9,46 +13,52 @@ import CardListScreen from './src/screens/card-list';
 
 const Tab = createBottomTabNavigator();
 
-const App = () => {
+const client = new ApolloClient({
+  uri: GRAPHQL_URI,
+});
+
+const App: FC = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
-        }}>
-        <Tab.Screen
-          name={`${Screens.Home}`}
-          component={CategoryListStackScreen}
-          options={{
-            title: 'Categories',
-            tabBarIcon: ({focused, color, size}) => (
-              <Icon name="list" size={size} color={color} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name={`${Screens.CardList}`}
-          component={CardListScreen}
-          options={{
-            title: 'My Credit Cards',
-            tabBarIcon: ({focused, color, size}) => (
-              <Icon name="credit-card" size={size} color={color} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name={`${Screens.Search}`}
-          component={ManageScreen}
-          options={{
-            title: 'Manage',
-            tabBarIcon: ({focused, color, size}) => (
-              <Icon name="edit" size={size} color={color} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Tab.Navigator
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          }}>
+          <Tab.Screen
+            name={`${Screens.Home}`}
+            component={CategoryListStackScreen}
+            options={{
+              title: 'Categories',
+              tabBarIcon: ({focused, color, size}) => (
+                <Icon name="list" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name={`${Screens.CardList}`}
+            component={CardListScreen}
+            options={{
+              title: 'My Credit Cards',
+              tabBarIcon: ({focused, color, size}) => (
+                <Icon name="credit-card" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name={`${Screens.Search}`}
+            component={ManageScreen}
+            options={{
+              title: 'Manage',
+              tabBarIcon: ({focused, color, size}) => (
+                <Icon name="edit" size={size} color={color} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 };
 
